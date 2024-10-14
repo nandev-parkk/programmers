8434f349b758d90d29cfd14f65b9588e71309228
nandev <nandev.parkk@gmail.com>
2024-10-14 23:33:52 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.24 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.30 ms
+메모리: 33.4 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 11일 22:11:32
+2024년 10월 14일 23:33:49
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.js
@@ -1,95 +1,43 @@
 function solution(a, b, c, d) {
-  const numArr = [a, b, c, d];
-  const dice = [1, 2, 3, 4, 5, 6];
+  const nums = [a, b, c, d];
 
-  const map = new Map();
+  const numCount = {};
 
-  for (let i = 0; i < numArr.length; i++) {
-    for (let j = 0; j < dice.length; j++) {
-      if (numArr[i] === dice[j]) {
-        if (!map.has("p") || (map.has("p") && map.get("p").num === numArr[i])) {
-          map.set("p", {
-            num: numArr[i],
-            length: map.get("p") ? ++map.get("p").length : 1,
-          });
-        } else if (
-          !map.has("q") ||
-          (map.has("q") && map.get("q").num === numArr[i])
-        ) {
-          map.set("q", {
-            num: numArr[i],
-            length: map.get("q") ? ++map.get("q").length : 1,
-          });
-        } else if (
-          !map.has("r") ||
-          (map.has("r") && map.get("r").num === numArr[i])
-        ) {
-          map.set("r", {
-            num: numArr[i],
-            length: map.get("r") ? ++map.get("r").length : 1,
-          });
-        } else if (
-          !map.has("s") ||
-          (map.has("s") && map.get("s").num === numArr[i])
-        ) {
-          map.set("s", {
-            num: numArr[i],
-            length: map.get("s") ? ++map.get("s").length : 1,
-          });
-        }
-      }
-    }
+  for (const num of nums) {
+    numCount[num] ? (numCount[num] = numCount[num] + 1) : (numCount[num] = 1);
   }
 
+  const sortedKeys = Object.entries(numCount)
+    .sort(([, a], [, b]) => {
+      return b - a;
+    })
+    .reduce((acc, [key]) => [...acc, +key], []);
+
   let answer;
-  const mapSize = map.size;
 
-  if (mapSize === 1) {
+  if (sortedKeys.length === 1) {
     /** 네개가 모두 같은 경우 1111 * p */
+    answer = +sortedKeys * 1111;
+  } else if (sortedKeys.length === 2) {
+    const firstKey = sortedKeys[0];
+    const secondKey = sortedKeys[1];
 
-    const { num } = map.get("p");
-    answer = 1111 * num;
-  } else if (mapSize === 2) {
-    const { num: pNum, length: pLength } = map.get("p");
-    const { num: qNum, length: qLength } = map.get("q");
-
-    if (pLength === 3 || qLength === 3) {
+    if (numCount[firstKey] === numCount[secondKey]) {
       /** 세개(p)가 같고 하나(q)가 다른 경우 (10 * p + q) * 2 */
-      answer =
-        pLength > qLength ? (10 * pNum + qNum) ** 2 : (10 * qNum + pNum) ** 2;
+      answer = (firstKey + secondKey) * Math.abs(firstKey - secondKey);
     } else {
       /** 두개(p, q)씩 같은 경우 (p + q) * |p - q| */
-
-      answer = (pNum + qNum) * Math.abs(pNum - qNum);
+      answer = (10 * firstKey + secondKey) ** 2;
     }
-  } else if (mapSize === 3) {
+  } else if (sortedKeys.length === 3) {
+    const secondKey = sortedKeys[1];
+    const thirdKey = sortedKeys[2];
+
     /** 두개(p)가 같고 나머지(q, r)가 다른 경우 q * r */
-    const { num: pNum } = map.get("p");
-    const { num: qNum } = map.get("q");
-    const { num: rNum } = map.get("r");
-
-    let mostLengthKey;
-
-    map.forEach((value, key) => {
-      if (value.length === 2) {
-        mostLengthKey = key;
-      }
-    });
-
-    if (mostLengthKey === "p") answer = qNum * rNum;
-    if (mostLengthKey === "q") answer = pNum * rNum;
-    if (mostLengthKey === "r") answer = pNum * qNum;
-  } else if (mapSize === 4) {
+    answer = secondKey * thirdKey;
+  } else if (sortedKeys.length === 4) {
     /** 모두 다른 경우 가장 작은 숫자 */
-    const values = map.values();
-
-    const valueArr = [];
-
-    for (const value of values) {
-      valueArr.push(value.num);
-    }
-
-    answer = Math.min(...valueArr);
+    answer = Math.min(...sortedKeys);
   }
 
   return answer;
