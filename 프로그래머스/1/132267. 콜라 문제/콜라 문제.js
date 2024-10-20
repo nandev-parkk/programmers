d87172a7eba9155ff91ec8e74d2781516862bc1e
nandev <nandev.parkk@gmail.com>
2024-10-20 16:36:17 +0900

    [level 1] Title: 콜라 문제, Time: 0.23 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -4,11 +4,9 @@ function solution(a, b, n) {
   while (n >= a) {
     // 주는 갯수
     const sentCoke = n - (n % a);
-    console.log(sentCoke, "sentCoke");
 
     // 받는 갯수
     const receivedCoke = (sentCoke / a) * b;
-    console.log(receivedCoke, "receivedCoke");
 
     answer += receivedCoke;
 
@@ -16,6 +14,5 @@ function solution(a, b, n) {
     n = n - sentCoke + receivedCoke;
   }
 
-  console.log(answer, "answer");
   return answer;
 }
