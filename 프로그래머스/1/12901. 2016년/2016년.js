97eab1327f1d24de2b3b9a0ab6bd36fb32d1e31a
nandev <nandev.parkk@gmail.com>
2024-11-04 21:02:44 +0900

    [level 1] Title: 2016년, Time: 281.59 ms, Memory: 34.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-  const year = 2016;
-  const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-
-  const answer = days[new Date(year, a - 1, b).getDay()];
-
-  return answer;
+  return new Date(2016, a - 1, b).toString().slice(0, 3).toUpperCase();
 }
