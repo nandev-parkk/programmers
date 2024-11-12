dd9681316491eeeba25f978193843554192a8c0f
nandev <nandev.parkk@gmail.com>
2024-11-12 21:53:53 +0900

    [level 1] Title: 소수 만들기, Time: 6.04 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,30 +1,30 @@
 function solution(nums) {
-const arr = [];
+const sumNums = [];
 
   nums.forEach((num, i) => {
     for (let j = i + 1; j < nums.length; j++) {
       for (let k = j + 1; k < nums.length; k++) {
         const sum = nums[i] + nums[j] + nums[k];
 
-        console.log(sum, "sum");
-
-        arr.push(sum);
+        sumNums.push(sum);
       }
     }
   });
 
-  const a = arr
+  const answer = sumNums
     .map((num) => {
       let count = 0;
       const sqrt = Math.floor(Math.sqrt(num));
 
       for (let i = 2; i <= sqrt; i++) {
+        if (count > 0) return 0;
+
         num % i === 0 && count++;
       }
 
       return count > 0 ? 0 : num;
     })
-    .filter((num) => !!num);
+    .filter((num) => num);
 
-  return a.length;
+  return answer.length;
 }
