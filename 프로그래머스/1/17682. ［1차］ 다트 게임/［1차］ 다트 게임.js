c953eb5144a507d965f1388570ffc31e624dc80c
nandev <nandev.parkk@gmail.com>
2024-12-17 00:48:39 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.57 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,48 @@
+function solution(dartResult) {
+    const bonus = {
+    S: 1,
+    D: 2,
+    T: 3,
+  };
+
+  const option = {
+    "*": " * 2",
+    "#": " * (-1)",
+  };
+
+  const splitDartResult = dartResult.match(/\d+|[^\d]/g);
+
+  const numIndex = splitDartResult
+    .map((cv, i) => (isNaN(cv) ? cv : i))
+    .filter((num) => !isNaN(num));
+
+  for (let i = 0; i < splitDartResult.length; i++) {
+    const currentValue = splitDartResult[i];
+
+    if (Number(currentValue)) continue;
+
+    if (currentValue === "S" || currentValue === "D" || currentValue === "T") {
+      splitDartResult[i - 1] =
+        `${splitDartResult[i - 1]} ** ${bonus[currentValue]}`;
+    }
+
+    if (currentValue === "*") {
+      const sliceNumIndex = numIndex.filter((num) => i > num).slice(-2);
+
+      sliceNumIndex.forEach((index) => {
+        splitDartResult[index] += option[currentValue];
+      });
+    }
+
+    if (currentValue === "#") {
+      splitDartResult[i - 2] += option[currentValue];
+    }
+  }
+
+  const answer = splitDartResult
+    .filter((cv) => /\d/.test(cv))
+    .map((cv) => new Function(`return ${cv}`)())
+    .reduce((acc, cv) => acc + cv, 0);
+
+  return answer;
+}
