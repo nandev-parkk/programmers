a77b66526c4019bc75a4e8bb28546efd184e4e57
nandev <nandev.parkk@gmail.com>
2024-12-25 21:40:10 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 46.84 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,9 @@
+function solution(arr) {
+  return arr.map((num) => {
+    if (num % 2 === 0 && num >= 50) {
+      return num / 2;
+    } else if (num % 2 !== 0 && num < 50) {
+      return num * 2;
+    } else return num;
+  });
+}
