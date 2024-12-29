12b8be06b4e474ed911373352f7c8c6a1317fbb0
nandev <nandev.parkk@gmail.com>
2024-12-29 16:22:31 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.71 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -0,0 +1,34 @@
+function solution(board, moves) {
+  const basket = [];
+  let count = 0;
+
+  for (let i = 0; i < moves.length; i++) {
+    const craneIndex = moves[i] - 1;
+
+    for (let j = 0; j < board.length; j++) {
+      const boardIndex = board[j][craneIndex];
+
+      // board에 인형이 없으면 continue
+      if (!boardIndex) continue;
+
+      if (boardIndex) {
+        basket.push(boardIndex);
+        board[j][craneIndex] = 0;
+
+        break;
+      }
+    }
+
+    const lastNum = basket[basket.length - 1];
+    const lastPrevNum = basket[basket.length - 2];
+      
+          if (!lastNum || !lastPrevNum) continue;
+
+    if (lastNum === lastPrevNum) {
+      basket.splice(-2, 2);
+      count += 2;
+    }
+  }
+    
+    return count;
+}
