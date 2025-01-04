ac7061034618a9cffbc0a6827fbe6b271eb97dc3
nandev <nandev.parkk@gmail.com>
2025-01-04 23:59:02 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 1.01 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] === arr[j][i]) continue;
+
+      if (arr[i][j] !== arr[j][i]) {
+        return 0;
+      }
+    }
+  }
+
+  return 1;
+}
