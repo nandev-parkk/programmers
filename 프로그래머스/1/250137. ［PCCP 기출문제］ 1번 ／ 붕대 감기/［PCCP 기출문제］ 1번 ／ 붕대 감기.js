59265ec0775058506687c5f52d5feb180180edb3
nandev <nandev.parkk@gmail.com>
2025-01-06 22:41:25 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 5.29 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.js
@@ -0,0 +1,39 @@
+function solution(bandage, health, attacks) {
+  const [successSkillTime, recoveryPerSecond, additionalRecovery] = bandage;
+  let tempHealth = health;
+
+  // attacks의 0번 인덱스의 공격 시간을 시작시간, 마지막 인덱스의 공격 시간을 종료 시간으로 지정
+  const [startTime] = attacks[0];
+  const [endTime] = attacks[attacks.length - 1];
+
+  let successRecovery = 0;
+
+  for (let i = startTime; i <= endTime; i++) {
+      if (tempHealth < 0) return -1;
+      
+    const currentAttack = attacks.find(([attackTime]) => attackTime === i);
+
+    if (currentAttack) {
+      const [, damage] = currentAttack;
+
+      tempHealth -= damage;
+      successRecovery = 0;
+
+      continue;
+    }
+
+    // 1초당 회복량 만큼 회복
+    tempHealth = tempHealth >= health ? health : tempHealth + recoveryPerSecond;
+
+    // 연속 성공 여부 판별을 위해 1씩 플러스
+    successRecovery++;
+
+    if (successRecovery === successSkillTime) {
+      tempHealth =
+        tempHealth >= health ? health : tempHealth + additionalRecovery;
+      successRecovery = 0;
+    }
+  }
+
+  return tempHealth > 0 ? tempHealth : -1;
+}
