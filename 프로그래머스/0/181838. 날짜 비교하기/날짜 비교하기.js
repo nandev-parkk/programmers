03af560ef0a43d158534444b172961bc0f01caaa
nandev <nandev.parkk@gmail.com>
2025-01-07 14:57:48 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.19 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,6 +1,3 @@
 function solution(date1, date2) {
-  const convertedDate1 = new Date(date1.join("-"));
-  const convertedDate2 = new Date(date2.join("-"));
-
-  return convertedDate1 < convertedDate2 ? 1 : 0;
+    return new Date(date1) < new Date(date2) ? 1 : 0;
 }
