a3ff712ccd2b21722f4eb105ef71e602ec6fa970
nandev <nandev.parkk@gmail.com>
2025-01-13 23:19:19 +0900

    [level 1] Title: 신고 결과 받기, Time: 868.13 ms, Memory: 95.3 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,32 +1,36 @@
 function solution(id_list, report, k) {
   // report에서 신고당한 id를 key,  {신고 당한 횟수: ..., 신고자: []} value로 객체를 만든다.
-
-  const obj = {};
+  const reportCounts = {};
 
   for (const ids of report) {
     const [reporter, respondent] = ids.split(" ");
 
     // reporters에 reporter가 이미 포함되어 있는 경우 건너뛰기
-    if (respondent in obj && obj[respondent].reporters.includes(reporter))
+    if (
+      respondent in reportCounts &&
+      reportCounts[respondent].reporters.includes(reporter)
+    )
       continue;
 
-    obj[respondent] = {
-      count: ((respondent in obj && obj[respondent].count) || 0) + 1,
+    reportCounts[respondent] = {
+      count:
+        ((respondent in reportCounts && reportCounts[respondent].count) || 0) +
+        1,
       reporters:
-        respondent in obj
-          ? [...obj[respondent].reporters, reporter]
+        respondent in reportCounts
+          ? [...reportCounts[respondent].reporters, reporter]
           : [reporter],
     };
   }
 
-  const filteredObj = Object.values(obj)
+  const filteredReportCounts = Object.values(reportCounts)
     .filter(({ count }) => count >= k)
     .map(({ reporters }) => reporters);
 
   const answer = id_list.map((id) => {
     let count = 0;
 
-    for (const reporters of filteredObj) {
+    for (const reporters of filteredReportCounts) {
       reporters.includes(id) && count++;
     }
 
