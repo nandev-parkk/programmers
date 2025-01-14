9ce3e0e0fc355f33403dc67cb1a6cd1316810b06
nandev <nandev.parkk@gmail.com>
2025-01-14 10:37:23 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 1024.84 ms, Memory: 45.5 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.js
@@ -1,4 +1,7 @@
 function solution(friends, gifts) {
+  // 각 사람이 받을 선물 갯수를 구하기 위해 각 사람의 이름을 키로 객체 만들기
+  const answer = {};
+
   // 각 사람의 선물 지수 구하기
   const giftIndex = gifts.reduce((acc, cv) => {
     const [giver, receiver] = cv.split(" ");
@@ -17,6 +20,8 @@ function solution(friends, gifts) {
   for (const gift of gifts) {
     const [giver, receiver] = gift.split(" ");
 
+    answer[giver] = 0;
+
     // giver(본인)을 제외한 친구들 객체 생성
     const excludeGiver = friends
       .filter((friend) => friend !== giver)
@@ -34,13 +39,6 @@ function solution(friends, gifts) {
     }
   }
 
-  // 각 사람이 받을 선물 갯수를 구하기 위해 각 사람의 이름을 키로 객체 만들기
-  const answer = {};
-
-  for (const friend of friends) {
-    answer[friend] = 0;
-  }
-
   // 객체를 만들어서 [a][b] [b][a] 비교
   const giftCountsEntries = Object.entries(giftCounts);
 
@@ -50,7 +48,7 @@ function solution(friends, gifts) {
     const receivers = Object.keys(counts);
 
     for (const receiver of receivers) {
-      // receiver가 없는 경우(괘씸하게도 선물을 아예 안한 친구들) giver를 + 1하고 continue
+      // receiver가 없는 경우(괘씸하게도 선물을 아예 안한 친구들) giver + 1하고 continue
       if (!giftCounts[receiver]) {
         answer[giver]++;
 
