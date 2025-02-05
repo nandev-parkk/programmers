b0edf3d6d55a4cb8328a230ba167c5c48df5c564
nandev <nandev.parkk@gmail.com>
2025-02-05 16:47:52 +0900

    [level 2] Title: 튜플, Time: 62.40 ms, Memory: 43 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,5 +1,7 @@
 function solution(s) {
-  return convertedString = s
+  const answer = [];
+
+  const convertedString = s
     .split("},{")
     .map((cv) =>
       cv
@@ -7,6 +9,13 @@ function solution(s) {
         .split(",")
         .map((str) => +str),
     )
-    .sort((a, b) => a.length - b.length)
-    .reduce((acc, cv) => acc.concat(cv.filter((v) => !acc.includes(v))), []);
+    .sort((a, b) => a.length - b.length);
+
+  for (const nums of convertedString) {
+    for (const num of nums) {
+      !answer.includes(num) && answer.push(num);
+    }
+  }
+
+  return answer;
 }
