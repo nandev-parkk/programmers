1d00b20a9dd07c408fbf6fbf21698a3e5b518763
nandev <nandev.parkk@gmail.com>
2025-02-07 23:12:26 +0900

    [level 2] Title: [1차] 캐시, Time: 35.87 ms, Memory: 42.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -0,0 +1,27 @@
+function solution(cacheSize, cities) {
+  if (!cacheSize) return cities.length * 5;
+    
+  const cache = [];
+  let runtime = 0;
+
+  for (const city of cities) {
+    const toLowerCaseCity = city.toLowerCase();
+
+    if (!cache.includes(toLowerCaseCity)) {
+      if (cache.length < cacheSize) {
+        cache.push(toLowerCaseCity);
+      } else {
+        cache.shift();
+        cache.push(toLowerCaseCity);
+      }
+
+      runtime += 5;
+    } else {
+      cache.splice(cache.indexOf(toLowerCaseCity), 1);
+      cache.push(toLowerCaseCity);
+      runtime++;
+    }
+  }
+
+  return runtime;
+}
