b965e6bfaed595d89f1ae6d280ab50d993589de6
nandev <nandev.parkk@gmail.com>
2025-02-07 14:07:14 +0900

    [level 2] Title: 튜플, Time: 11.94 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,15 +1,9 @@
 function solution(s) {
 const answer = new Set();
-
-    const convertedString = s
-    .split("},{")
-    .map((cv) =>
-      cv
-        .replace(/[\{\}]/g, "")
-        .split(",")
-        .map((str) => +str),
-    )
-    .sort((a, b) => a.length - b.length);
+    
+      const convertedString = JSON.parse(
+    s.replace(/{/g, "[").replace(/}/g, "]"),
+  ).sort((a, b) => a.length - b.length);
     
       for (const nums of convertedString) {
     for (const num of nums) {
