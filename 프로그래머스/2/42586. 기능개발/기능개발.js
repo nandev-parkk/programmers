24ca21b549bae1bd49dd38a9f74734ea65452ec6
nandev <nandev.parkk@gmail.com>
2025-02-07 13:33:45 +0900

    [level 2] Title: 기능개발, Time: 4.77 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -0,0 +1,31 @@
+function solution(progresses, speeds) {
+  const answer = [];
+
+  const ca = progresses.map((progress, i) => {
+    let count = 0;
+
+    while (progress < 100) {
+      progress += speeds[i];
+      count++;
+    }
+
+    return count;
+  });
+
+  let start = 0;
+  let end = 1;
+
+  while (start < ca.length) {
+    if (ca[start] >= ca[end]) {
+      end++;
+    } else {
+      answer.push(ca.slice(start, end).length);
+      start = end;
+      end++;
+    }
+  }
+
+  console.log(answer);
+
+  return answer;
+}
