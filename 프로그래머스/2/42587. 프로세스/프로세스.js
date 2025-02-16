f003e6ad08e95a6c5be11e9087bbd614efba2323
nandev <nandev.parkk@gmail.com>
2025-02-17 01:02:16 +0900

    [level 2] Title: 프로세스, Time: 0.81 ms, Memory: 34 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,24 @@
+function solution(priorities, location) {
+    const TARGET_PRIORITY = priorities[location];
+
+  priorities[location] = "target";
+
+  let answer = 0;
+
+  while (priorities.includes("target")) {
+    const currentPriority =
+      priorities[0] === "target" ? TARGET_PRIORITY : priorities[0];
+
+    const copiedPriorities = [...priorities];
+    copiedPriorities[copiedPriorities.indexOf("target")] = TARGET_PRIORITY;
+
+    if (Math.max(...copiedPriorities.slice(1)) > currentPriority)
+      priorities.push(priorities.shift());
+    else {
+      priorities.splice(0, 1);
+      answer++;
+    }
+  }
+
+  return answer;
+}
