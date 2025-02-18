5639cd6aceae794982150e8a174667b7b60c6cf7
nandev <nandev.parkk@gmail.com>
2025-02-18 09:14:25 +0900

    [level 2] Title: 타겟 넘버, Time: 13.61 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,4 +1,18 @@
 function solution(numbers, target) {
-    var answer = 0;
-    return answer;
+  let answer = 0;
+
+  function dfs(i, sum) {
+    if (i === numbers.length) {
+      sum === target && answer++;
+
+      return;
+    }
+
+    dfs(i + 1, sum + numbers[i]);
+    dfs(i + 1, sum - numbers[i]);
+  }
+
+  dfs(0, 0);
+
+  return answer;
 }
