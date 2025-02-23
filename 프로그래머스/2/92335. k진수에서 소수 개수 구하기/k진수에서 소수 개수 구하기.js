47d05d70b22f536a2a81ba049a2cd167a4834c20
nandev <nandev.parkk@gmail.com>
2025-02-24 00:23:45 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 9.00 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -0,0 +1,19 @@
+function solution(n, k) {
+  const answer = n
+    .toString(k)
+    .split("0")
+    .filter((v) => v)
+    .filter((v) => +v > 1 && checkIsPrime(+v));
+
+  return answer.length;
+}
+
+function checkIsPrime(num) {
+  if (num === 2) return true;
+
+  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+    if (!(num % i)) return false;
+  }
+
+  return true;
+}
