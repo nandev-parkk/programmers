70aa41e2b9a655e2293ee3453ea67760806736ec
nandev <nandev.parkk@gmail.com>
2025-03-17 23:09:16 +0900

    [level 2] Title: 숫자 변환하기, Time: 2639.68 ms, Memory: 68.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -0,0 +1,18 @@
+function solution(x, y, n) {
+  const queue = [[y, 0]];
+
+  while (queue.length) {
+    const [num, i] = queue.shift();
+
+    if (num === x) return i;
+
+
+    if (num % 2 === 0 && num / 2 >= x) queue.push([num / 2, i + 1]);
+
+    if (num % 3 === 0 && num / 3 >= x) queue.push([num / 3, i + 1]);
+      
+      if (num - n >= x) queue.push([num - n, i + 1]);
+  }
+
+  return -1;
+}
