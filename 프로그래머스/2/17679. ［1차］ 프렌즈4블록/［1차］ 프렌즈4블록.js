9ef8b124ee19bcdff48dc8dbfce147feecaf148f
nandev <nandev.parkk@gmail.com>
2025-03-24 12:53:03 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 30.66 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,21 +1,21 @@
 function solution(m, n, board) {
   // board의 각 요소가 문자열이라 치환이 안되므로 배열로 변경
-  const mappedBorad = board.map((v) => [...v]);
+  const mappedBoard = board.map((v) => [...v]);
 
   const deleteList = [];
 
   while (true) {
     // 각 요소마다 [row, col], [row, col + 1], [row + 1, col], [row + 1, col + 1] 총 4개의 요소를 검사해 4개가 같으면 deleteList에 현재 요소 좌표 push
-    for (let row = 0; row < mappedBorad.length; row++) {
-      for (let col = 0; col < mappedBorad[row].length; col++) {
-        const currentColBlock = mappedBorad[row][col];
+    for (let row = 0; row < mappedBoard.length; row++) {
+      for (let col = 0; col < mappedBoard[row].length; col++) {
+        const currentColBlock = mappedBoard[row][col];
 
         // 현재 요소가 없거나 높이와 폭이 board의 경계인 경우 계산할 필요가 없으므로 continue
         if (!currentColBlock || row === m - 1 || col === n - 1) continue;
 
-        const nextColBlock = mappedBorad[row][col + 1];
-        const nextRowCurrentColBlock = mappedBorad[row + 1][col];
-        const nextRowNextColBlock = mappedBorad[row + 1][col + 1];
+        const nextColBlock = mappedBoard[row][col + 1];
+        const nextRowCurrentColBlock = mappedBoard[row + 1][col];
+        const nextRowNextColBlock = mappedBoard[row + 1][col + 1];
 
         // 4개의 block이 모두 일치하는지 확인
         const set = new Set([
@@ -29,16 +29,16 @@ function solution(m, n, board) {
       }
     }
 
-    if (!deleteList.length) return mappedBorad.flat().filter((v) => !v).length;
+    if (!deleteList.length) return mappedBoard.flat().filter((v) => !v).length;
 
     // deleteList에 요소가 있으면 제거
     while (deleteList.length) {
       const [row, col] = deleteList.shift();
 
-      mappedBorad[row][col] = "";
-      mappedBorad[row][col + 1] = "";
-      mappedBorad[row + 1][col] = "";
-      mappedBorad[row + 1][col + 1] = "";
+      mappedBoard[row][col] = "";
+      mappedBoard[row][col + 1] = "";
+      mappedBoard[row + 1][col] = "";
+      mappedBoard[row + 1][col + 1] = "";
     }
 
     // 아래에서 위의 요소를 당겨와야 풀림
@@ -59,8 +59,8 @@ function solution(m, n, board) {
         colIndex++;
       }
 
-      const currentBlock = mappedBorad[rowIndex][colIndex];
-      const nextBlock = mappedBorad[nextRowIndex][colIndex];
+      const currentBlock = mappedBoard[rowIndex][colIndex];
+      const nextBlock = mappedBoard[nextRowIndex][colIndex];
 
       if (currentBlock) {
         rowIndex--;
@@ -69,11 +69,11 @@ function solution(m, n, board) {
       } else {
         if (nextBlock) {
           [
-            mappedBorad[rowIndex][colIndex],
-            mappedBorad[nextRowIndex][colIndex],
+            mappedBoard[rowIndex][colIndex],
+            mappedBoard[nextRowIndex][colIndex],
           ] = [
-            mappedBorad[nextRowIndex][colIndex],
-            mappedBorad[rowIndex][colIndex],
+            mappedBoard[nextRowIndex][colIndex],
+            mappedBoard[rowIndex][colIndex],
           ];
 
           rowIndex--;
