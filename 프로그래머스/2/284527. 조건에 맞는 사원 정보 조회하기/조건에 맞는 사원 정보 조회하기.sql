3d6e86915cdcd3f2ebd44c2d16aa98c93a3cb757
nandev <nandev.parkk@gmail.com>
2025-04-29 10:04:43 +0900

    [level 2] Title: 조건에 맞는 사원 정보 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/284527. 조건에 맞는 사원 정보 조회하기/조건에 맞는 사원 정보 조회하기.sql
@@ -0,0 +1,19 @@
+-- 코드를 작성해주세요
+SELECT S.SCORE, S.EMP_NO, E.EMP_NAME, E.POSITION, E.EMAIL
+FROM
+    (
+        SELECT EMP_NO, SUM(SCORE) AS SCORE
+        FROM HR_GRADE
+        GROUP BY EMP_NO
+    ) S
+    JOIN HR_EMPLOYEES E
+    ON S.EMP_NO = E.EMP_NO
+WHERE S.SCORE IN(
+    SELECT MAX(M.SCORE)
+    FROM
+        (
+            SELECT SUM(SCORE) AS SCORE
+            FROM HR_GRADE
+            GROUP BY EMP_NO
+        ) M
+    )
