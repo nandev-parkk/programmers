b5a2d54e324b61d78b759e7f25e53dd98539147f
nandev <nandev.parkk@gmail.com>
2025-07-07 18:46:14 +0900

    [level 2] Title: 요격 시스템, Time: 436.02 ms, Memory: 167 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.js
@@ -1,16 +1,15 @@
 function solution(targets) {
   const sortedTargets = targets.sort((a, b) => a[1] - b[1]);
 
-  // for문을 돌면서 다음꺼랑 안겹치면 + 1
   let count = 0;
-  let endPoint = 0;
+  let interceptionPoint = 0;
 
   for (let i = 0; i < sortedTargets.length; i++) {
     let [currentStart, currentEnd] = sortedTargets[i];
 
-    if (currentStart >= endPoint) {
+    if (currentStart >= interceptionPoint) {
       count++;
-      endPoint = currentEnd;
+      interceptionPoint = currentEnd;
     }
   }
 
