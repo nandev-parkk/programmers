7d3e77459148ab04d92f85eb4fb618aa03a3d869
nandev <nandev.parkk@gmail.com>
2025-07-22 10:52:04 +0900

    [level 2] Title: 숫자 블록, Time: 134.78 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
+++ 프로그래머스/2/12923. 숫자 블록/숫자 블록.js
@@ -1,28 +1,4 @@
-function getDivisor(num) {
-  if (num === 1) return 0;
-
-  let temp = 0;
-
-  for (let i = 2; i <= Math.ceil(Math.sqrt(num)); i++) {
-    if (num % i === 0) {
-      if (Math.round(num / i) > 10000000) {
-        temp = i;
-        continue;
-      }
-
-      return Math.round(num / i);
-    }
-  }
-
-  return temp || 1;
-}
-
 function solution(begin, end) {
-  const answer = [];
-
-  for (let i = begin; i <= end; i++) {
-    answer.push(getDivisor(i));
-  }
-
-  return answer;
+    var answer = [];
+    return answer;
 }
