a0710c57ef1c190a92ccd504742b35524d8ce02e
nandev <nandev.parkk@gmail.com>
2025-07-29 10:07:22 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 54.32 ms, Memory: 43.6 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,18 +1,47 @@
 function solution(cap, n, deliveries, pickups) {
-      let answer = 0;
-  let delivery = 0;
-  let pickup = 0;
+  let answer = 0;
 
-  for (let i = n - 1; i >= 0; i--) {
-    delivery += deliveries[i];
-    pickup += pickups[i];
-
-    while (delivery > 0 || pickup > 0) {
-      delivery -= cap;
-      pickup -= cap;
-      answer += (i + 1) * 2;
-    }
+  // 가장 먼 곳에 배달해야하거나 수거해야할 택배가 없는 경우 제거
+  while (deliveries[n - 1] === 0 && pickups[n - 1] === 0) {
+    deliveries.pop();
+    pickups.pop();
+    n--;
   }
 
-  return answer;
+  let distance = n;
+
+  let truck = 0;
+
+  // 배달해야하거나 수거해야할 택배가 있는 경우
+  while (deliveries.length || pickups.length) {
+    // 배달해야할 택배가 있는 경우
+    while (deliveries.length) {
+      truck += deliveries.pop();
+
+      if (truck > cap) {
+        deliveries.push(truck - cap);
+
+        break;
+      }
+    }
+    truck = 0;
+
+    // 수거해야할 택배가 있는 경우
+    while (pickups.length) {
+      truck += pickups.pop();
+
+      if (truck > cap) {
+        pickups.push(truck - cap);
+
+        break;
+      }
+    }
+    truck = 0;
+
+    // answer에 distance를 더하고, distance를 배달 및 수거 후 가장 먼 곳으로 최신화
+    answer += distance;
+    distance = Math.max(deliveries.length, pickups.length);
+  }
+
+  return answer * 2;
 }
