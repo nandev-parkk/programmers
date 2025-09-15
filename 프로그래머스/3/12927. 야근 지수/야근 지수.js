6f79c4384c8c7b4786db9ec71df90dd37b73e216
nandev <nandev.parkk@gmail.com>
2025-09-15 15:25:06 +0900

    [level 3] Title: 야근 지수, Time: 37.61 ms, Memory: 36.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.js
@@ -0,0 +1,20 @@
+function solution(n, works) {
+  // 퇴근 전까지 다 처리할 수 있는 경우
+  if (works.reduce((acc, cv) => acc + cv, 0) <= n) return 0;
+
+  const sortedWorks = works.sort((a, b) => b - a);
+
+  while (n) {
+    let maxWork = sortedWorks[0];
+
+    for (let i = 0; i < sortedWorks.length; i++) {
+      if (!n) break;
+      if (sortedWorks[i] < maxWork) continue;
+
+      sortedWorks[i] -= 1;
+      n--;
+    }
+  }
+
+  return works.reduce((acc, cv) => (acc += cv ** 2), 0);
+}
