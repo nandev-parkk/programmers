8e97de9fe4622e55239346a1cb820566a5817469
nandev <nandev.parkk@gmail.com>
2025-09-23 10:24:13 +0900

    [level 3] Title: 등굣길, Time: 1.20 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -0,0 +1,32 @@
+function solution(m, n, puddles) {
+  const dp = Array.from({ length: n }, () => Array(m).fill(0));
+
+  dp[0][0] = 1;
+
+  const puddleMap = Array.from({ length: n }, () => Array(m).fill(false));
+
+  for (const [puddleX, puddleY] of puddles) {
+    puddleMap[puddleY - 1][puddleX - 1] = true;
+  }
+  // [
+  //  [ 1, 1, 1, 1],
+  //  [ 1, 1, 1, 1],
+  //  [ 1, 1, 1, 1]
+  // ]
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (puddleMap[i][j]) continue;
+
+      if (!i && j) dp[i][j] = dp[i][j] + dp[i][j - 1];
+
+      if (!j && i) dp[i][j] = dp[i - 1][j] + dp[i][j];
+
+      if (i && j) {
+        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+      }
+    }
+  }
+
+  return dp[n - 1][m - 1];
+}
