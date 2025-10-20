99b8f995fb6f1a2aeae9ab9687ba927e69a5c2d5
nandev <nandev.parkk@gmail.com>
2025-10-20 10:26:47 +0900

    [level 3] Title: 베스트앨범, Time: 0.56 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -1,20 +1,11 @@
 function solution(genres, plays) {
+  // map 데이터 구조: [genres]: [ [total], [ [id], [count], [id], [count] ] ]
   const map = new Map();
 
-  // map 데이터 구조
-  // [genres]: {
-  //  total: [count],
-  //  data: [
-  //   [id], [count],
-  //   [id], [count],
-  //   ...
-  //  ]
-  // }
   for (let i = 0; i < genres.length; i++) {
     const genre = genres[i];
     const count = plays[i];
 
-    // if (!map.has(genre)) map.set(genre, { total: 0, data: [] });
     if (!map.has(genre)) map.set(genre, [0, []]);
 
     const [total, data] = map.get(genre);
@@ -23,15 +14,20 @@ function solution(genres, plays) {
     map.set(genre, [total + count, data]);
   }
 
-  const sorted = [...map.entries()].sort((a, b) => b[1][0] - a[1][0]);
+  // 장르별 속한 노래들의 전체 재생수 내림차순
+  const sortedByTotalCount = [...map.entries()].sort(
+    (a, b) => b[1][0] - a[1][0],
+  );
 
   const answer = [];
-  sorted.forEach(([_, data]) => {
-    const a = data[1].sort((a, b) => b[1] - a[1] || a[0] - b[0]).slice(0, 2);
 
-    a.forEach(([idx]) => {
-      answer.push(idx);
-    });
+  // 장르별 속한 노래들의 각 재생수 내림차순, 재생수가 같은 경우 인덱스 오름차순 후 앞 2개만 자름
+  sortedByTotalCount.forEach(([_, data]) => {
+    const sortedBySongCount = data[1]
+      .sort((a, b) => b[1] - a[1] || a[0] - b[0])
+      .slice(0, 2);
+
+    sortedBySongCount.forEach(([idx]) => answer.push(idx));
   });
 
   return answer;
