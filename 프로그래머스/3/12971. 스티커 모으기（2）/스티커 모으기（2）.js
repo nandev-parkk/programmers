b1caee3cacdc41bba0dd60e49bd65c0a6353afdc
nandev <nandev.parkk@gmail.com>
2025-10-20 21:28:23 +0900

    [level 3] Title: 스티커 모으기(2), Time: 20.71 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.js
@@ -3,6 +3,8 @@ function solution(sticker) {
   // [i - 2]: 전전 스티커까지 뜯었을 때의 최댓값
   // [i - 2] + sticker[i]: 인접한 스티커가 아니므로 현재 스티커를 뜯을 수 있다.
 
+  if (sticker.length === 1) return sticker[0];
+    
   // i가 음수가 되므로 + 2
   const length = sticker.length + 2;
 
@@ -11,8 +13,6 @@ function solution(sticker) {
 
   // 두번째부터 떼기
   const dp2 = Array.from({ length }, () => 0);
-    
-  if (sticker.length === 1) return sticker[0];
 
   for (let i = 2; i < length - 1; i++) {
     dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + sticker[i - 2]);
