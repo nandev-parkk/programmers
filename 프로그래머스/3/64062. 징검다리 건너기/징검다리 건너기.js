b789f6f2aa2643a0844c681b6e06dff99caa7d7d
nandev <nandev.parkk@gmail.com>
2026-01-12 13:45:19 +0900

    [level 3] Title: 징검다리 건너기, Time: 59.66 ms, Memory: 43.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.js
@@ -0,0 +1,22 @@
+function solution(stones, k) {
+  let [left, right] = [1, 200000000];
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    let count = 0;
+
+    // stones의 각 원소에서 mid를 빼는 코드
+    for (const stone of stones) {
+      if (stone - mid <= 0) count++;
+      else count = 0;
+
+      if (count >= k) break;
+    }
+
+    if (count >= k) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return left;
+}
