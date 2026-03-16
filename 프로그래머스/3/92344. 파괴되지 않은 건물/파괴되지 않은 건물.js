91687b13d5e6ef2056e186c53a1982ca6ecd74c2
nandev <nandev.parkk@gmail.com>
2026-03-16 15:46:12 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 188.77 ms, Memory: 186 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -31,6 +31,7 @@ function solution(board, skill) {
     }
   }
 
+  // 전체 계산
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
       board[i][j] += copied[i][j];
