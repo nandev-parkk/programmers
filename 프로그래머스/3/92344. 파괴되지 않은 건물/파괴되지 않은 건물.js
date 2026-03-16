23a88a69913b193e228dbc5c63dca4fe2f9312e6
nandev <nandev.parkk@gmail.com>
2026-03-16 15:45:20 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 192.43 ms, Memory: 185 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.js
@@ -0,0 +1,47 @@
+function solution(board, skill) {
+  const copied = Array.from({ length: board.length + 1 }, () =>
+    Array.from({ length: board[0].length + 1 }, () => 0),
+  );
+
+  for (const [type, r1, c1, r2, c2, degree] of skill) {
+    if (type === 1) {
+      copied[r1][c1] -= +degree;
+      copied[r1][c2 + 1] -= -degree;
+      copied[r2 + 1][c1] -= -degree;
+      copied[r2 + 1][c2 + 1] -= +degree;
+    } else {
+      copied[r1][c1] += +degree;
+      copied[r1][c2 + 1] += -degree;
+      copied[r2 + 1][c1] += -degree;
+      copied[r2 + 1][c2 + 1] += +degree;
+    }
+  }
+
+  // 위에서 아래로 누적합
+  for (let i = 1; i < copied.length; i++) {
+    for (let j = 0; j < copied[0].length; j++) {
+      copied[i][j] = copied[i - 1][j] + copied[i][j];
+    }
+  }
+
+  // 왼쪽에서 오른쪽으로 누적합
+  for (let i = 0; i < copied.length; i++) {
+    for (let j = 1; j < copied[0].length; j++) {
+      copied[i][j] = copied[i][j - 1] + copied[i][j];
+    }
+  }
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      board[i][j] += copied[i][j];
+    }
+  }
+
+  const answer = board.reduce((acc, cur) => {
+    const filtered = cur.filter((num) => num > 0);
+
+    return acc + filtered.length;
+  }, 0);
+
+  return answer;
+}
